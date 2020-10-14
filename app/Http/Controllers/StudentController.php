ce4d4ee1076e4cbca923d8553060d479fafc1206
LucaRiccio <lucariccio888@gmail.com>
2020-10-14 19:07:50 +0200

    Consegna esercizio (aggiornato con tabelle Laravel/Mysql)

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,31 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Boolean;
 
 class StudentController extends Controller
 {
 
-  protected $students;
-
-  public function __construct(){
-    $this->students = config('students');
-    // $this->getAllStudent();
-  }
-
-  // public function getAllStudent(){
-  //   $this->students = config('students');
-  // }
-
   public function studenti(){
-    $data = $this->students;
+    $data = Boolean::all();
     return view('studenti',compact('data'));
   }
 
-  public function show($id){
-    if (!array_key_exists($id,$this->students)){
-      abort(404);
-    }
-    $student = $this->students[$id];
-    return view('show',compact('student'));
-  }
+  // protected $students;
+  //
+  // public function __construct(){
+  //   $this->students = config('students');
+  //   // $this->getAllStudent();
+  // }
+  //
+  // // public function getAllStudent(){
+  // //   $this->students = config('students');
+  // // }
+  //
+  // public function studenti(){
+  //   $data = $this->students;
+  //   return view('studenti',compact('data'));
+  // }
+  //
+  // public function show($id){
+  //   if (!array_key_exists($id,$this->students)){
+  //     abort(404);
+  //   }
+  //   $student = $this->students[$id];
+  //   return view('show',compact('student'));
+  // }
 }
--- resources/views/studenti.blade.php
+++ resources/views/studenti.blade.php
@@ -13,13 +13,40 @@
   <div class="card-group">
     @foreach ($data as $key=> $student)
     <div class="card">
-      <img src="{{$student['img']}}" class="card-img-top" alt="{{$student['nome']}}">
+      <img src="{{$student->img}}" class="card-img-top" alt="{{$student->nome}}">
       <div class="card-body">
-        <a href="{{ route('Student.show',['id' => $key])}}"><h5 class="card-title">{{$student['nome']}} ({{$student['eta']}} anni)</h5></a> 
-        <p class="card-text">Assunt{{($student['genere'] == 'f' ? 'a' : 'o')}} da {{$student['azienda']}}</p>
-        {{-- <p class="card-text descrizione">{{$student['descrizione']}}</p> --}}
+        <a href="{{ route('Student.show',['id' => $student->id])}}"><h5 class="card-title">{{$student->nome}} ({{$student->anni}} anni)</h5></a>
+        <p class="card-text">Assunt{{($student->genere == 'f' ? 'a' : 'o')}} da {{$student->azienda}}</p>
+        <p class="card-text descrizione">{{$student->descrizione}}</p>
       </div>
     </div>
     @endforeach
 </div>
 @endsection
+
+
+{{-- @extends('layouts.app')
+
+@section('titolo','studenti')
+
+@section('content')
+
+  <div class="titolo-studenti text-center">
+    <h2>I nostri ex-studenti su LinkedIn</h2>
+    <h4>Tu ci metti l’impegno, noi tutta la trasparenza. Siamo pronti a scommettere su di te.</h4>
+
+  </div>
+
+  <div class="card-group">
+    @foreach ($data as $key=> $student)
+    <div class="card">
+      <img src="{{$student['img']}}" class="card-img-top" alt="{{$student['nome']}}">
+      <div class="card-body">
+        <a href="{{ route('Student.show',['id' => $key])}}"><h5 class="card-title">{{$student['nome']}} ({{$student['eta']}} anni)</h5></a>
+        <p class="card-text">Assunt{{($student['genere'] == 'f' ? 'a' : 'o')}} da {{$student['azienda']}}</p>
+        {{-- <p class="card-text descrizione">{{$student['descrizione']}}</p> --}}
+{{--      </div>
+    </div>
+    @endforeach
+</div>
+@endsection --}}
